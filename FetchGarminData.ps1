36104f7e66e70f44b6adcddeb62e6e269b2dc583
Mats Wigle <mats@opsmode.com>
2020-12-29 18:49:54 +0100

    first one

---
+++ FetchGarminData.ps1
@@ -0,0 +1,42 @@
+import-module GarminConnect -Force
+
+$config = Get-Content .\Configuration.json -Raw | ConvertFrom-Json
+
+
+$password = $config.Password | ConvertTo-SecureString -AsPlainText -Force
+
+$credential = New-Object System.Management.Automation.PSCredential -ArgumentList $config.UserName, $password
+
+$login = New-GarminConnectLogin -Credential $credential
+
+if ($login) {
+
+    $sleepData = Get-GarminSleepData -UserDisplayName $config.DisplayName -Date "2020-12-28"
+
+    if ($sleepData.dailySleepDTO.lightSleepSeconds -gt 0) {
+        
+        # Convert date to Unix timestamp with nanoseconds
+        [datetime]$date = $sleepData.dailySleepDTO.calendarDate
+        $timeStamp = [long]((New-TimeSpan -Start (Get-Date -Date '1970-01-01') -End (($Date).ToUniversalTime())).TotalSeconds * 1E9)
+
+        $userProfile = $sleepData.dailySleepDTO.userProfilePK
+
+        $sleep = @{
+            SleepTimeSeconds = $sleepData.dailySleepDTO.sleepTimeSeconds
+            UnmeasurableSleepSeconds = $sleepData.dailySleepDTO.unmeasurableSleepSeconds
+            DeepSleepSeconds = $sleepData.dailySleepDTO.deepSleepSeconds
+            LightSleepSeconds = $sleepData.dailySleepDTO.lightSleepSeconds
+            REMSleepSeconds = $sleepData.dailySleepDTO.remSleepSeconds
+            AwakeSleepSeconds = $sleepData.dailySleepDTO.awakeSleepSeconds
+        }
+
+        foreach ($metric in $sleep.Keys) {
+            
+            $value = $sleep.$metric
+
+            Invoke-RestMethod -Uri 'http://localhost:8086/write?db=garmin' -Method POST -Body "$metric,UserProfile=$userProfile value=$value $timeStamp"
+        }
+    }
+}
+
+
