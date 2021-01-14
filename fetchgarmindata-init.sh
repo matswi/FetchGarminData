1efc1ef2987d5ffa3486aebd62a80e828b9ad4b7
mats wigle <mats.wigle@basefarm.com>
2021-01-14 14:54:50 +0100

    local time

--- fetchgarmindata-init.sh
+++ fetchgarmindata-init.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -e
 docker build --tag fetchgarmindata https://github.com/matswi/FetchGarminData/raw/master/Dockerfile
-docker run --hostname $(hostname) --name="fetchgarmindata" --interactive --rm --tty --privileged -v /home/pi/FetchGarminData/Configuration.json:/root/FetchGarminData/Configuration.json fetchgarmindata
+docker run --hostname $(hostname) --name="fetchgarmindata" --interactive --rm --tty --privileged -v /home/pi/FetchGarminData/Configuration.json:/root/FetchGarminData/Configuration.json -v /etc/localtime:/etc/localtime:ro fetchgarmindata
